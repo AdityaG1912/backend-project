fc18f642816911cacae0750ff44bc10fd4b14d39
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-04-01 02:09:59 +0530

    db_connecting

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error");
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
