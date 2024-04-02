66e1f7b3b4e06e7151b50a7198e2f0574db1e8bb
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-04-03 01:53:52 +0530

    accepting json and cors is added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed", err);
+  });
 
 // import express from "express";
-
 // const app = express();
-
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
