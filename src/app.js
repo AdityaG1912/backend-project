72d33246577ee07fc351c86dffe310b7026de0f0
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-04-03 13:55:40 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -10,11 +10,18 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb ",
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
