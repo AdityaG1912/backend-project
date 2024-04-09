5a8f588bdf6f8cdf292b69fd2c1748c7cacbb01c
AdityaG1912 <aditya.gaikwad2013@gmail.com>
2024-04-09 20:43:34 +0530

    all setuup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,5 @@
-import mongoose from "mongoose";
-
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
